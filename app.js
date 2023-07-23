d429230f901a37d68b2c5b244fc046894a001e76
Lucien Lin <luciensday@gmail.com>
2023-07-23 23:33:31 +0100

    Roman converter complete

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 
-function convertToRoman(numberInput) {
+function convertToRoman(decimalNumber) {
 
     const romanList = {
-        M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1}
+        M:1000, 
+        CM:900, 
+        D:500, 
+        CD:400, 
+        C:100, 
+        XC:90, 
+        L:50, 
+        XL:40, 
+        X:10, 
+        IX:9, 
+        V:5, 
+        IV:4, 
+        I:1
+    }
 
     let output = ""
+
     for (let i in romanList) {
-    while (numberInput >= romanList[i]) {
+    while (decimalNumber >= romanList[i]) {
         output += i; 
-        number -= romanList[i]
+        decimalNumber -= romanList[i]
     }
     }
 
     return output; 
+
+   
 }
 
 function handleConvert() {
@@ -20,14 +36,22 @@ function handleConvert() {
 const numberInput= document.getElementById('number-input')
 const result = document.getElementById('result-div')
 
-const romanNumeral  = convertToRoman(numberInput)
-result.textContent = `Roman Numeral: ${RomanNumeral}`
+const decimalNumber= parseInt (numberInput.value)
+
+  // Validate the input
+  if (isNaN(decimalNumber) || decimalNumber <= 0 || decimalNumber > 3999) {
+    result.textContent = 'Please enter a valid number (1 to 3999)';
+    return;
+}
+
+const romanNumeral  = convertToRoman(decimalNumber)
+result.textContent = `Roman Numeral: ${romanNumeral}`
 
 } 
 
 
 
-const submit = document.getElementbyId('sumbit')
-submit.addEventListener ('click', handleConvert)
+const submit = document.getElementById('submit');
+submit.addEventListener('click', handleConvert);
 
 
