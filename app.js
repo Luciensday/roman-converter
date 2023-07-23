05c6ae2c9d6c44aa01519fa9ea956283a2665f8a
Lucien Lin <luciensday@gmail.com>
2023-07-23 22:22:39 +0100

    add roman function

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-let numberInput= document.getElementById('number-input')
-let submit =document.getElementById('submit')
 
-submit.addEventListener('click', )
+function convertToRoman(numberInput) {
+
+    const romanList = {
+        M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1}
+
+    let output = ""
+    for (let i in romanList) {
+    while (numberInput >= romanList[i]) {
+        output += i; 
+        number -= romanList[i]
+    }
+    }
+
+    return output; 
+}
+
+
+
+const numberInput= document.getElementById('number-input')
+const result = document.getElementById('result-div')
+
+
+
